eaadf16b29af78940e046e686bdaaeb49e16b37d
vips <vipashadutta123@gmail.com>
2022-12-02 10:10:18 +0000

    Create 3 files

---
+++ vips.js
@@ -0,0 +1,54 @@
+console.log ("Vipasha is Coding");
+console.log(typeof 23455);
+console.log(typeof 23536732673457467);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(23455);
+console.log(23536732673457467);
+console.log(true);
+console.log(false);
+console.log(undefined);
+console.log(null);
+const x = 'vipasha ';
+var y = 'is ';
+let z = 'coding';
+z = 'eating';
+console.log(x+y+z);
+const a = 10;
+const b = 12;
+if(null){
+    console.log(' a is greater ')
+}else{
+    console.log(' b is greater ')
+}
+const n = 10;
+for (let i = 0; i< n ; i++){
+    console.log(i);
+};
+try {
+    throw new Error("Vipasha is boring")
+} catch (err) {
+    console.log(err);
+}
+function square(x) {
+    return x*x;
+}
+console.log(
+    square(4)
+)
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "❤️❤️❤️",
+    T : "V "
+}));
